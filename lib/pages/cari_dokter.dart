1bd567204b070ce542bebe79a6db93efcd1b9c4d
boyaditya <boy.adityarr@gmail.com>
2024-04-20 16:21:56 +0700

    first commit

---
+++ lib/pages/cari_dokter.dart
@@ -0,0 +1,229 @@
+import 'package:flutter/material.dart';
+
+class CariDokter extends StatefulWidget {
+  const CariDokter({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<CariDokter> createState() => _CariDokterState();
+}
+
+class _CariDokterState extends State<CariDokter> {
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, String> data =
+        (ModalRoute.of(context)?.settings.arguments as Map<String, String>?) ??
+            {};
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text(
+        'Cari Dokter',
+        style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      )),
+      body: ListView(
+        children: [
+          const Divider(
+            color: Colors.black,
+            thickness: 0.2,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 55,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextFormField(
+                    initialValue: data['namaDokter'],
+                    decoration: const InputDecoration(
+                      hintText: 'Cari nama dokter atau spesialisasi',
+                      hintStyle: TextStyle(fontSize: 14),
+                      suffixIcon: Icon(Icons.search),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                CustomButton(
+                    icon: Icons.filter_alt, text: "Filter", onPressed: () {}),
+                const SizedBox(height: 20),
+                const Text(
+                  'Dokter',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                CustomButton2(
+                  icon: Icons.filter_alt,
+                  dokter: "dr. John Doe, MARS, SpAk",
+                  spesialis: "Andrologi - Spesialis Andrologi",
+                  availability: "Tersedia hari ini",
+                  imagePath: 'assets/images/dokter/dummy-doctor.jpg',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/profil_dokter');
+                  },
+                ),
+                const SizedBox(height: 10),
+                CustomButton2(
+                  icon: Icons.filter_alt,
+                  dokter: "dr. John Doe, MARS, SpAk",
+                  spesialis: "Andrologi - Spesialis Andrologi",
+                  availability: "Tersedia hari ini",
+                  imagePath: 'assets/images/dokter/dummy-doctor.jpg',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/profil_dokter');
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final VoidCallback onPressed;
+
+  const CustomButton({
+    super.key,
+    required this.icon,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        width: 80,
+        decoration: BoxDecoration(
+          color: Colors.grey[200],
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(8),
+              child: Icon(
+                icon,
+                size: 20,
+              ),
+            ),
+            Text(
+              text,
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton2 extends StatelessWidget {
+  final IconData icon;
+  final String dokter;
+  final String spesialis;
+  final String availability;
+  final String imagePath;
+  final VoidCallback onPressed;
+
+  const CustomButton2({
+    super.key,
+    required this.icon,
+    required this.dokter,
+    required this.spesialis,
+    required this.availability,
+    required this.imagePath,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        foregroundColor: Colors.blue,
+        disabledForegroundColor: Colors.grey.withOpacity(0.38),
+        disabledBackgroundColor: Colors.grey.withOpacity(
+            0.12), // This changes the color when the button is pressed
+        padding: const EdgeInsets.all(6),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        side: const BorderSide(color: Colors.grey, width: 0.3),
+      ),
+      child: Row(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(8),
+            child: CircleAvatar(
+              radius: 40,
+              backgroundImage: AssetImage(imagePath),
+            ),
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                dokter,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 6),
+              Text(
+                spesialis,
+                style: const TextStyle(
+                  fontSize: 12,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 4),
+              Row(
+                children: [
+                  const Icon(
+                    Icons.check_circle,
+                    color: Colors.green,
+                    size: 20,
+                  ),
+                  const SizedBox(width: 4),
+                  Text(
+                    availability,
+                    style: const TextStyle(
+                      fontSize: 12,
+                      color: Colors.black,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/profil_pasien.dart
@@ -0,0 +1,195 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Quiz UI Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Dashboard(title: 'Quiz UI Flutter'),
+    );
+  }
+}
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Profil Pasien',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        ),
+      ),
+      body: ListView(
+        children: [
+          const Divider(color: Colors.black),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "SAYA SENDIRI",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 10),
+                const CustomContainer(
+                  icon: Icons.filter_alt,
+                  nama: "John Hendrick",
+                  birthday: "7 Maret 2001",
+                  email: "johnhendrick@gmail.com",
+                  noHP: "08123456789",
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  "ORANG LAIN",
+                  style: TextStyle(color: Colors.grey),
+                ),
+                const SizedBox(height: 90),
+                const Center(
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.groups,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        "Anda belum menambahkan profil untuk orang lain",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 120),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width,
+                      40,
+                    ),
+                  ),
+                  child: const Text('Selanjutnya',
+                      style: TextStyle(color: Colors.white)),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomContainer extends StatefulWidget {
+  final IconData icon;
+  final String nama;
+  final String birthday;
+  final String email;
+  final String noHP;
+
+  const CustomContainer({
+    super.key,
+    required this.icon,
+    required this.nama,
+    required this.birthday,
+    required this.email,
+    required this.noHP,
+  });
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _CustomContainerState createState() => _CustomContainerState();
+}
+
+class _CustomContainerState extends State<CustomContainer> {
+  bool _isToggled = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          _isToggled = !_isToggled;
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: _isToggled ? Colors.blue : Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey,
+              blurRadius: 4.0,
+              offset: Offset(-2, 2),
+            ),
+          ],
+        ),
+        width: double.infinity,
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              widget.nama,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              widget.birthday,
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              widget.email,
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 6),
+            Text(
+              widget.noHP,
+              style: const TextStyle(
+                fontSize: 12,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
