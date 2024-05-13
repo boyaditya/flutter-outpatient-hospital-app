b7b39370900779b9a895b41c167474decfa7e679
arulzkash <arulzkash@gmail.com>
2024-05-13 11:33:40 +0700

    mulai webservice

--- web_service/main.py
+++ web_service/main.py
@@ -2,7 +2,7 @@ from os import path
 
 from fastapi import Depends, Request, FastAPI, HTTPException, status
 
-# from fastapi.responses import FileResponse
+# from fastapi.responses import FileResponser
 # from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
@@ -57,3 +57,17 @@ async def create_user(user: schemas.UserCreate, db: db_dependency):
 async def read_doctors(db: db_dependency):
     return crud.get_doctors(db)
 
+
+@app.get("/users/{user_id}", response_model=schemas.User)
+async def read_user(user_id: int, db: db_dependency):
+    user = crud.get_user_by_id(db, user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+@app.delete("/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, db: db_dependency):
+    deleted_user = crud.delete_user_by_id(db, user_id)
+    if deleted_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
